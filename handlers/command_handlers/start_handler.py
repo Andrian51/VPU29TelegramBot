f50151a579cc1f65440183e43c3b7b666b7ff3c9
Andrian Diduh <diduhandrian8@icloud.com>
2024-04-11 11:20:10 +0300

    Added .env.example

--- handlers/command_handlers/start_handler.py
+++ handlers/command_handlers/start_handler.py
@@ -20,6 +20,6 @@ class StartHandler(BaseHandler):
 
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="I'm a bot, please talk to me!",
+            text="Привіт Андріян",
             reply_markup=reply_markup
         )
